3005f21d211f6d2a0dd1e6dc1cace1e92dae6b80
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-15 15:11:17 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] strArray = text.Split();
+            string max = strArray[0];
+            foreach (string item in strArray)
+            {
+                if (max.Length < item.Length)
+                {
+                    max = item;
+                }
+               
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+         
+        {
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "").ToLower();
+            string str2 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            if (str == str2)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
